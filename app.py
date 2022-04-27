a9cf903c1a418950e04500c01374ea9e79e3942c
Jkenyut <satrianursaputro06@gmail.com>
2022-04-27 16:00:36 +0700

    Update: CV and Speech Recognition

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.utils import secure_filename
 # fungsi buatan sendiri -> menyesuaikan
 #from employee_analysis import *
 
+
 # file upload
 UPLOAD_FOLDER = 'static/video-uploaded/'
 ALLOWED_EXTENSIONS = {'mp4'}
@@ -13,12 +14,16 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # validation file upload
+
+
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Halaman awal
-@app.route('/', methods = ['POST', 'GET'])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     video = None
     # jika ada request file
@@ -28,9 +33,11 @@ def index():
     if video and allowed_file(video.filename):
         video_filename = secure_filename(video.filename)
         video.save(os.path.join(app.config['UPLOAD_FOLDER'], video_filename))
+
         video = UPLOAD_FOLDER+video.filename
-        #data = analysis(video) # menampilkan hasil dari analisis
+        # data = analysis(video) # menampilkan hasil dari analisis
     return render_template('index.html', video=video)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- employee_analysis.py
+++ employee_analysis.py
@@ -1,59 +1,144 @@
+from cgitb import text
+import os
+import cv2
+import speech_recognition as sr
+import pandas as pd
+import numpy as np
+import face_recognition
+from datetime import datetime
+import moviepy.editor as mp
+r = sr.Recognizer()
+
 # fungsi merubah mp4 ke mp3/wav
+
+# melakukan encoding gambar di folder untuk face recognition
+path = 'karyawan'
+images = []
+classNames = []
+myList = os.listdir(path)
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        try:
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        except IndexError as e:
+            print(e)
+    return encodeList
+
+
+encodeListKnown = findEncodings(images)
+
+
 def video_to_audio(path):
-	# drop code untuk mengubah video ke audio
-	# ...
-	# ...
-	path = # isi dengan path ke audio disimpan
-	return path # mengembalikan nilai path
+    # drop code untuk mengubah video ke audio
+    clip = mp.VideoFileClip('static/video-uploaded/'+path)
+    clip.audio.write_audiofile(r"static/audio-uploaded/"+path+".wav")
+    return "static/audio-uploaded/"+path+".wav"  # mengembalikan nilai path
 
 # fungsi merubah audio ke teks
-def audio_to_teks(path):
-	#drop code untuk mengubah audio ke teks
-	# ...
-	# ...
-	teks = # isi dengan teks hasil dari audio
-	return teks # mengembalikan nilai teks
+
+
+def audio_to_teks(path, lang='id-ID'):
+    # drop code untuk mengubah audio ke teks
+    with sr.AudioFile(path) as source:
+        # print('Fetching File')
+        audio_file = r.record(source)
+    text = r.recognize_google(audio_file, language=lang)
+    return text
 
 # fungsi prediksi perasaan
+
+
 def predict_feeling(path):
-	# mp4 to wav/mp3
-	vid2aud = video_to_audio(path)
-	# audio to teks
-	aud2text = audio_to_teks(vid2aud)
-	# prediksi
-	feel = # hasil prediksi perasaan
-	return aud2text, feel
-	
+    # mp4 to wav/mp3
+    vid2aud = video_to_audio(path)
+    # audio to teks
+    aud2text = audio_to_teks(vid2aud)
+    # prediksi
+    # feel = # hasil prediksi perasaan
+    return aud2text
+
 # fungsi untuk identifikasi wajah karyawan
+
+
+def faces_encode(img):
+    facesCurFrame = face_recognition.face_locations(img)
+    encodesCurFrame = face_recognition.face_encodings(img, facesCurFrame)
+    zipped_faces_encode = zip(encodesCurFrame, facesCurFrame)
+    return facesCurFrame, zipped_faces_encode
+
+
 def employee_identity(path):
-	# drop code untuk mengidentifikasi wajah karyawan
-	# ...
-	# ...
-	return id_karyawan # mengembalikan id karyawan
+    # drop code untuk mengidentifikasi wajah karyawan
+    cap = cv2.VideoCapture(path)
+    run = (True)
+    while run:
+        success, frame = cap.read()
+        img = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+        facesCurFrame, zipped_faces_encode = faces_encode(img)
+        if not facesCurFrame:
+            img1 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            facesCurFrame, zipped_faces_encode = faces_encode(img1)
+        if not facesCurFrame:
+            img2 = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+            facesCurFrame, zipped_faces_encode = faces_encode(img2)
+        for encodeFace, faceLoc in zipped_faces_encode:
+            matches = face_recognition.compare_faces(
+                encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(
+                encodeListKnown, encodeFace)
+            # print(faceDis)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].lower()
+                print(name)
+                if name in ['satria', 'satria1']:
+                    id_karyawan = name
+                    print(id_karyawan)
+                    run = (False)
+                else:
+                    print('none')
+    return id_karyawan  # mengembalikan id karyawan
 
 # menambahkan data hasil analisis video dan teks
+
+
 def add_to_list(id, komentar, perasaan):
-  # mencari nilai id pada csv data karyawan
-  profile_akun = profile[profile['nama'] == id]
-  # ubah ke list
-  data = profile_akun.values.tolist()[0]
-  # datetime
-  now = datetime.now()
-  # tambahkkan data komentar dan perasaan ke list
-  data.append(komentar)
-  data.append(perasaan)
-  data.append(now.strftime("%m-%d-%Y, %H:%M:%S"))
-  return data
-	
+    # mencari nilai id pada csv data karyawan
+    profile_akun = profile[profile['nama'] == id]
+    # ubah ke list
+    data = profile_akun.values.tolist()[0]
+    # datetime
+    now = datetime.now()
+    # tambahkkan data komentar dan perasaan ke list
+    data.append(komentar)
+    data.append(perasaan)
+    data.append(now.strftime("%m-%d-%Y, %H:%M:%S"))
+    return data
+
 # fungsi utama
+
+
 def analysis(path):
-	# hasil identifikasi wajah
-	id_karyawan = employee_identity(path)
-	# hasil analisis komentar dan perasaan
-	komentar, perasaan = predict_feeling(path)
-	# hasil keseluruhan analisis
-	data = add_to_list(id, komentar, perasaan)
-	# tambah ke dataframe
-	data = pd.DataFrame([a], columns=data_classify_col)
-	data_klasifikasi = data_klasifikasi.append(data)
-	
+    #
+    # hasil identifikasi wajah
+    id_karyawan = employee_identity(path)
+    # hasil analisis komentar dan perasaan
+    komentar, perasaan = predict_feeling(path)
+    # hasil keseluruhan analisis
+    data = add_to_list(id, komentar, perasaan)
+    # tambah ke dataframe
+    data = pd.DataFrame([a], columns=data_classify_col)
+    data_klasifikasi = data_klasifikasi.append(data)
+
+
+employee_identity('static/video-uploaded/video_20220427_133336.mp4')
